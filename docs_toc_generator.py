6bd09bd564b7cd05f93d2d532b3841d59ba1d1aa
gou7ma7 <604109142@qq.com>
2021-07-31 10:55:57 +0800

    适配高贵的Windows用户

--- docs_toc_generator.py
+++ docs_toc_generator.py
@@ -1,5 +1,6 @@
 import os
 
+
 h1_list = []
 
 
@@ -9,11 +10,11 @@ def get_h1_list(doc_path):
             file_path = os.path.join(path, file_name)
             if file_name.endswith('.md'):  # 只处理.md
                 title = os.path.split(file_name)[1].split('.')[0]
-                h1_list.append(f'- [{title}]({file_path}#{title})')
+                h1_list.append(f'- [{title}]({file_path})')
                 h1_in_md = get_h1_line(file_path)
                 if h1_in_md:  # 过滤没有h1的
                     h1_list.append(
-                        f"{' ' * 2 * file_path.count('/')}"f'- [{h1_in_md}]({file_path}#{h1_in_md})')
+                        f"{' ' * 2 * file_path.count(os.sep)}" + f'- [{h1_in_md}]({file_path}#{h1_in_md})')
     return h1_list
 
 
